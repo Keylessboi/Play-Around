825a3a3da1da9b2d639f6b0b17c66236ec59fc99
Firelife2 <81932275+Firelife2@users.noreply.github.com>
2022-12-21 21:59:49 +0000

    chess

--- ChessAI/AI.py
+++ ChessAI/AI.py
@@ -11,21 +11,6 @@ image_filenames = ['img/black_bishop.png', 'img/black_king.png', 'img/black_knig
                    'img/white_bishop.png', 'img/white_king.png', 'img/white_knight.png', 'img/white_pawn.png', 'img/white_queen.png', 'img/white_rook.png']
 images = {}
 
-for filename in image_filenames:
-    try:
-        image = Image.open(filename)
-    except FileNotFoundError:
-        print(f'Error: image file {filename} not found.')
-        continue
-
-    # Create a transparent image to use as the alpha channel
-    alpha = Image.new('L', image.size, 255)
-    image.alpha_composite(alpha)
-    images[filename[:-4]] = image
-
-# Save the images with the removed background
-for filename, image in images.items():
-    image.save(filename + '_transparent.png')
 
 window_size = 400
 square_size = window_size // 8
