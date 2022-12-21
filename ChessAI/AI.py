a39300cfe65bd42552f7cab3ced0aac955b716eb
Firelife2 <81932275+Firelife2@users.noreply.github.com>
2022-12-21 21:57:33 +0000

    hi

--- ChessAI/AI.py
+++ ChessAI/AI.py
@@ -7,22 +7,6 @@ image_filenames = ['img/black_bishop.png', 'img/black_king.png', 'img/black_knig
                    'img/white_bishop.png', 'img/white_king.png', 'img/white_knight.png', 'img/white_pawn.png', 'img/white_queen.png', 'img/white_rook.png']
 images = {}
 
-for filename in image_filenames:
-    try:
-        image = Image.open(filename)
-    except FileNotFoundError:
-        print(f'Error: image file {filename} not found.')
-        continue
-
-    # Create a transparent image to use as the alpha channel
-    alpha = Image.new('L', image.size, 255)
-    image.alpha_composite(alpha)
-    images[filename[:-4]] = image
-
-# Save the images with the removed background
-for filename, image in images.items():
-    image.save(filename + '_transparent.png')
-
 window_size = 400
 square_size = window_size // 8
 
@@ -89,36 +73,31 @@ def draw_board():
                 elif piece == 'K':
                     image = images['white_king']
                 screen.blit(image, (j * square_size, i * square_size))
-
-draw_board()
-
+                
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-            pygame.quit()
-            sys.exit()
         elif event.type == pygame.MOUSEBUTTONDOWN:
             pos = pygame.mouse.get_pos()
             col = pos[0] 
             row = pos[1] 
             if selected_piece:
-                # Move the piece
                 board[selected_pos[0]][selected_pos[1]] = ' '
+                selected_pos = [row, col]
                 board[row][col] = selected_piece
                 selected_piece = None
-                selected_pos = None
                 draw_board()
+
             else:
-                # Select a piece
-                piece = board[row][col]
-                if piece != ' ':
+               piece = board[row][col]
+               if piece != ' ':
                     selected_piece = piece
                     selected_pos = (row, col)
         if selected_piece:
             # Draw a highlight around the selected piece
             pygame.draw.rect(screen, (255, 0, 0), (selected_pos[1] * square_size, selected_pos[0] * square_size, square_size, square_size), 4)
 
-        # Quit pygame
-        pygame.quit()
+# Quit pygame
+pygame.quit()
