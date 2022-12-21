13eff292bd8ec3304274c71b386f100332835a6d
Firelife2 <81932275+Firelife2@users.noreply.github.com>
2022-12-21 19:36:43 +0000

    Chessai

--- ChessAI/AI.py
+++ ChessAI/AI.py
@@ -1,29 +1,41 @@
 import pygame
+from PIL import Image
+import sys
+
+# Load the images of the chess pieces
+image_filenames = ['black_bishop.png', 'black_king.png', 'black_knight.png', 'black_pawn.png', 'black_queen.png', 'black_rook.png',
+                   'white_bishop.png', 'white_king.png', 'white_knight.png', 'white_pawn.png', 'white_queen.png', 'white_rook.png']
+images = {}
+
+for filename in image_filenames:
+    try:
+        image = Image.open(filename)
+    except FileNotFoundError:
+        print(f'Error: image file {filename} not found.')
+        continue
+
+    # Create a transparent image to use as the alpha channel
+    alpha = Image.new('L', image.size, 255)
+    image.alpha_composite(alpha)
+    images[filename[:-4]] = image
+
+# Save the images with the removed background
+for filename, image in images.items():
+    image.save(filename + '_transparent.png')
 
 window_size = 400
-square_size = window_size / 8
+square_size = window_size // 8
 
 pygame.init()
 
-pygame.display.set_caption('Chess Board')
+pygame.display.set_caption('Scuffed Chess ai')
 icon = pygame.Surface((1, 1))
 icon.set_alpha(0)
 pygame.display.set_icon(icon)
 screen = pygame.display.set_mode((window_size, window_size))
 screen.fill((255, 255, 255))
 
-black_bishop = pygame.image.load('black_bishop.png')
-black_king = pygame.image.load('black_king.png')
-black_knight = pygame.image.load('black_knight.png')
-black_pawn = pygame.image.load('black_pawn.png')
-black_queen = pygame.image.load('black_queen.png')
-black_rook = pygame.image.load('black_rook.png')
-white_bishop = pygame.image.load('white_bishop.png')
-white_king = pygame.image.load('white_king.png')
-white_knight = pygame.image.load('white_knight.png')
-white_pawn = pygame.image.load('white_pawn.png')
-white_queen = pygame.image.load('white_queen.png')
-white_rook = pygame.image.load('white_rook.png')
+images = {filename: pygame.image.load(filename + '_transparent.png') for filename in image_filenames}
 
 board = [
     ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'],
@@ -33,99 +45,96 @@ board = [
     [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
     [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
     ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
-    ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'],
+    ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R']
 ]
-
 
 selected_piece = None
 selected_pos = None
 
-
 def draw_board():
-    # Draw the board
-    for row in range(8):
-        for col in range(8):
-            if (row + col) % 2 == 0:
-                color = (255, 255, 255)
-            else:
-                color = (0, 0, 0)
-            pygame.draw.rect(screen, color, (col * square_size, row * square_size, square_size, square_size))
+    for i in range(8):
+        for j in range(8):
+            color = (255, 255, 255) if (i + j) % 2 == 0 else (0, 0, 0)
+            pygame.draw.rect(screen, color, (i * square_size, j * square_size, square_size, square_size))
+    for i in range(8):
+        for j in range(8):
+            piece = board[i][j]
+            if piece != ' ':
+                if piece.islower():
+                    color = (255, 255, 255)
+                else:
+                    color = (0, 0, 0)
+                if piece == 'p':
+                    image = images['black_pawn']
+                elif piece == 'r':
+                    image = images['black_rook']
+                elif piece == 'n':
+                    image = images['black_knight']
+                elif piece == 'b':
+                    image = images['black_bishop']
+                elif piece == 'q':
+                    image = images['black_queen']
+                elif piece == 'k':
+                    image = images['black_king']
+                elif piece == 'P':
+                    image = images['white_pawn']
+                elif piece == 'R':
+                    image = images['white_rook']
+                elif piece == 'N':
+                    image = images['white_knight']
+                elif piece == 'B':
+                    image = images['white_bishop']
+                elif piece == 'Q':
+                    image = images['white_queen']
+                elif piece == 'K':
+                    image = images['white_king']
+                screen.blit(image, (j * square_size, i * square_size))
 
-    # Draw the pieces
-    for row in range(8):
-        for col in range(8):
-            piece = board[row][col]
-            if piece == 'r':
-                screen.blit(black_rook, (col * square_size, row * square_size))
-            elif piece == 'n':
-                screen.blit(black_knight, (col * square_size, row * square_size))
-            elif piece == 'b':
-                screen.blit(black_bishop, (col * square_size, row * square_size))
-            elif piece == 'q':
-                screen.blit(black_queen, (col * square_size, row * square_size))
-            elif piece == 'k':
-                screen.blit(black_king, (col * square_size, row * square_size))
-            elif piece == 'p':
-                screen.blit(black_pawn, (col * square_size, row * square_size))
-            elif piece == 'p': 
-                screen.blit(black_pawn, (col * square_size, row * square_size))
-            elif piece == 'R':
-                screen.blit(white_rook, (col * square_size, row * square_size))
-            elif piece == 'N':
-                screen.blit(white_knight, (col * square_size, row * square_size))
-            elif piece == 'B':
-                screen.blit(white_bishop, (col * square_size, row * square_size))
-            elif piece == 'Q':
-                screen.blit(white_queen, (col * square_size, row * square_size))
-            elif piece == 'K':
-                screen.blit(white_king, (col * square_size, row * square_size))
-            elif piece == 'P':
-                screen.blit(white_pawn, (col * square_size, row * square_size))
+draw_board()
 
-            # Update the display
-pygame.display.update()
-
-# Run the game loop
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+            pygame.quit()
+            sys.exit()
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            # Check if a piece was clicked
             pos = pygame.mouse.get_pos()
             col = pos[0] // square_size
             row = pos[1] // square_size
-            if board[row][col] != ' ':
-                # Select the piece
-                selected_piece = board[row][col]
-                selected_pos = (row, col)
-            else:
-                # Deselect the piece
+            if selected_piece:
+                # Move the piece
+                board[selected_pos[0]][selected_pos[1]] = ' '
+                board[row][col] = selected_piece
                 selected_piece = None
                 selected_pos = None
+                draw_board()
+            else:
+                # Select a piece
+                piece = board[row][col]
+                if piece != ' ':
+                    selected_piece = piece
+                    selected_pos = (row, col)
         elif event.type == pygame.MOUSEBUTTONUP:
-            # Check if a piece was released
             pos = pygame.mouse.get_pos()
             col = pos[0] // square_size
             row = pos[1] // square_size
-        if selected_piece and selected_pos and (row, col) != selected_pos:
-            # Move the piece
-            board[selected_pos[0]][selected_pos[1]] = ' '
-            board[row][col] = selected_piece
-            selected_piece = None
-            selected_pos = None
-            draw_board()
+            if selected_piece and (row, col) != selected_pos:
+                # Move the piece
+                if selected_pos is not None:
+                    board[selected_pos[0]][selected_pos[1]] = ' '
+                    board[row][col] = selected_piece
+                    selected_piece = None
+                    selected_pos = None
+                    draw_board()
+                board[row][col] = selected_piece
+                selected_piece = None
+                selected_pos = None
+                draw_board()
+        if selected_piece:
+            # Draw a highlight around the selected piece
+            pygame.draw.rect(screen, (255, 0, 0), (selected_pos[1] * square_size, selected_pos[0] * square_size, square_size, square_size), 4)
 
-
-    # Draw a highlight around the selected piece, if any
-    if selected_piece:
-        pygame.draw.rect(screen, (255, 0, 0), (selected_pos[1] * square_size, selected_pos[0] * square_size, square_size, square_size), 4)
-
-    # Update the display
-    pygame.display.update()
-
-# Quit pygame
-pygame.quit()
-
-
+        # Quit pygame
+        pygame.quit()
